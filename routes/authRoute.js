9e79f8f236b9938187e4f8a077a46995196d7f3e
Neha Pawar <nehapawar2320@gmail.com>
2024-12-30 22:04:01 +0530

    Authorization of Admin

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,6 +1,6 @@
 import express from 'express'
-import {registerController} from '../controller/authController.js'
-
+import {registerController, loginController, testController} from '../controller/authController.js'
+import {isAdmin, requireSignIn} from '../middlewares/authMiddleware.js'
 //router object
 const router = express.Router()
 
@@ -9,5 +9,10 @@ const router = express.Router()
 
 router.post('/register',registerController)
 
+//LOGIN || POST
+router.post('/login', loginController)
+
+//test routes
+router.get('/test',requireSignIn,isAdmin, testController)
 
 export default router
