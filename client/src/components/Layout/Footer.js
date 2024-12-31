2de1fbc00fd5b906706dd0bafdb1bbe423a7ef41
Neha Pawar <nehapawar2320@gmail.com>
2024-12-31 10:50:23 +0530

    Created a react app for client

---
+++ client/src/components/Layout/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='bg-dark text-light p-3'>
+        
+        <h4 className='text-center'>
+            All Right Reserved &copy; Neha Pawar
+        </h4>
+    </div>
+  )
+}
+
+export default Footer
