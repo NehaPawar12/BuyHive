20a1515ae9a6c5ca0594e4ecb9b53ec173c2e25e
Neha Pawar <nehapawar2320@gmail.com>
2025-01-03 21:57:28 +0530

    Cart functionality

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -5,11 +5,14 @@ import { Checkbox, Radio } from "antd";
 import { Prices } from "../components/Prices";
 import { useNavigate } from "react-router-dom";
 import "./HomePage.css"; // Import CSS file for custom styling
+import { useCart } from "../context/cart";
+import toast from 'react-hot-toast'
 
 
 const HomePage = () => {
 
-  const navigate = useNavigate()
+  const navigate = useNavigate();
+  const [cart, setCart] = useCart();
   const [products, setProducts] = useState([]);
   const [categories, setCategories] = useState([]);
   const [checked, setChecked] = useState([]);
@@ -157,7 +160,19 @@ const HomePage = () => {
                   </p>
                   <p className="card-text"> $ {p.price}</p>
                   <button className="btn btn-primary ms-1 " onClick={() => navigate(`/product/${p.slug}`)}>More Details</button>
-                  <button className="btn btn-secondary ms-1">ADD TO CART</button>
+                  <button
+                    className="btn btn-secondary ms-1"
+                    onClick={() => {
+                      setCart([...cart, p]);
+                      localStorage.setItem(
+                        "cart",
+                        JSON.stringify([...cart, p])
+                      );
+                      toast.success("Item Added to cart");
+                    }}
+                  >
+                    ADD TO CART
+                  </button>
                 </div>
               </div>
             ))}
