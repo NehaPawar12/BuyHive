7c6852ff968c0d77e18ef423ae3a2afd97116c9b
Neha Pawar <nehapawar2320@gmail.com>
2025-01-05 16:31:10 +0530

    Filters resigning

--- client/src/components/Prices.js
+++ client/src/components/Prices.js
@@ -1,31 +1,37 @@
 export const Prices = [
-    {
+
+  {
       _id: 0,
+      name: "Rs. 10 to 100",
+      array: [10, 99],
+  },
+    {
+      _id: 1,
       name: "Rs. 100 to 500",
       array: [100, 500],
     },
     {
-      _id: 1,
+      _id: 2,
       name: "Rs. 600 to 1000",
       array: [600, 1000],
     },
     {
-      _id: 2,
+      _id: 3,
       name: "Rs. 1000 to 3000",
       array: [1001, 3000],
     },
     {
-      _id: 3,
+      _id: 4,
       name: "Rs. 3000 to 7000",
       array: [3001, 7000],
     },
     {
-      _id: 4,
+      _id: 5,
       name: "Rs. 7000 to 10000",
       array: [7000, 10000],
     },
     {
-      _id: 4,
+      _id: 6,
       name: "Rs. 10000 or more",
       array: [10001, 300000],
     },
